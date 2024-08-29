2aab5aa46a09abd0f941e9f62920ecb89c45c762
Leonardo Moreno <leu-profissional@hotmail.com>
2024-08-29 11:22:10 -0300

    adicionando os arquivos do Docker e implmentando algumas melhorias de cinfig e dependencias

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import express from 'express';
+import app from "./database/app";
+import 'dotenv';
 
-const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello, Elden Ring Builder!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.listen(PORT, () => console.log(
+  `Server running in port ${PORT}`
+));
