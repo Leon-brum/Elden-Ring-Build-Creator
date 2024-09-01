83242263095a0cd657a9d6a7558d9d6072435d8e
Leonardo Moreno <leu-profissional@hotmail.com>
2024-09-01 12:39:48 -0300

    criado a rota de usuarios, cadastro de usuarios e validacoes de acesso

---
+++ src/database/seeders/20240901151153-users.ts
@@ -0,0 +1,21 @@
+import { QueryInterface } from 'sequelize';
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface: QueryInterface) {
+    await queryInterface.bulkInsert('users',[
+      {
+        name:'leonardo',
+        role:'admin',
+        email:'admin@admin.com',
+        password: '$2y$10$HUOM8E0yNVp0p6r7GAYs0uU5erQ8d7KyyqLK1duYcyj0I/Dt5LkIq'
+        // senha: senha_admin
+      },
+    ])
+  },
+
+  async down (queryInterface: QueryInterface) {
+    await queryInterface.bulkDelete('users', {});
+  }
+};
